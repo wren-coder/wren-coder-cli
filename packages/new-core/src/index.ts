968c740f5ad1510bf87bf3eafd03b6067c47b08d
Ryan Krumbholz <krumbholz98@gmail.com>
2025-07-28 11:36:12 -0400

    export chat

--- packages/new-core/src/index.ts
+++ packages/new-core/src/index.ts
@@ -4,13 +4,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export { BaseAgent } from "./agents/base.js";
-export { CoderAgent } from "./agents/coder.js";
-export { PlannerAgent } from "./agents/planner.js";
-export { SupervisorAgent } from "./agents/supervisor.js";
-
-export { CODER_PROMPT, PLANNER_PROMPT, SUPERVISOR_PROMPT } from "./prompts/index.js";
-
-export { handoffTool } from "./tools/supervisor/handoff.js";
-
 export { Chat } from "./chat.js";
